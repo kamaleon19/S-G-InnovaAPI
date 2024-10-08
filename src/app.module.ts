008feba518515c7792642d8575bab4bcaceaa9e2
kamaleon19 <joaco.px@gmail.com>
2024-10-08 19:44:28 -0300

    Proyecto iniciado y DB conectada

---
+++ src/app.module.ts
@@ -0,0 +1,27 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AppController } from './app.controller';
+import { AppService } from './app.service';
+
+@Module({
+  imports: [
+    ConfigModule.forRoot(),
+
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST,
+      port: +process.env.DB_PORT,
+      database: process.env.DB_NAME,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      autoLoadEntities: true,
+      synchronize: true // Al estar en un contexto de desarrollo o prueba tecnica dejo el synchronize en true.
+    }),
+
+  ],
+  controllers: [AppController],
+  providers: [AppService],
+})
+export class AppModule {}
